1ea5e70c7ced88102ac02b8d47d91c652c8378bd
victor <vitimm144@gmail.com>
2014-08-20 22:32:40 -0300

    Adicionado configuração do postgres

--- laboratorio/settings.py
+++ laboratorio/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login',
+    'cadastro',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,8 +59,12 @@ WSGI_APPLICATION = 'laboratorio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'laboratorio',
+        'USER': 'laboratorio',
+        'PASSWORD': 'laboratorio',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 

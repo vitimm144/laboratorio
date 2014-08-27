553b3f58b10b117622bb114cc68f81eaa2125e87
alpombo <alex@insidesolucoes.com.br>
2014-08-27 20:35:23 -0300

    Corrigido parte de persistencia de cadastro de paciente

--- cadastro/models.py
+++ cadastro/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Paciente(models.Model):
 
-    numero_paciente = models.CharField(verbose_name='Numero do Paciente')
-    data_registro = models.DateField(verbose_name='Data de Registro')
+    numero_paciente = models.IntegerField(verbose_name='Numero do Paciente', unique=True, auto_created=True)
+    data_registro = models.DateField(verbose_name='Data de Registro', auto_now=True)
     nome = models.CharField(max_length=200, verbose_name='Nome')
     GENDER_CHOICES = (
         (u'M', u'Masculino'),
@@ -13,26 +13,26 @@ class Paciente(models.Model):
     sexo = models.CharField(max_length=2, choices=GENDER_CHOICES, verbose_name='Sexo')
     data_nascimento = models.DateField(verbose_name='Data de Nascimento')
     nome_mae = models.CharField(max_length=200, verbose_name='Nome da Mãe')
-    cep = models.ForeignKey()
+    cep = models.CharField(max_length=10, null=True, verbose_name='CEP')
     endereco = models.CharField(max_length=200, verbose_name='Endereço')
     numero = models.CharField(max_length=20, verbose_name='numero')
     complemento = models.CharField(max_length=100, verbose_name='Compl.')
-    bairro = models.CharField(max_length=50, verbUose_name='Bairro')
-    Cidade = models.CharField(max_length=100, verbose_name='Cidade')
-    UF = models.BRStatefield(verbose_name='2')
+    bairro = models.CharField(max_length=50, verbose_name='Bairro')
+    cidade = models.CharField(max_length=100, verbose_name='Cidade')
+    uf = models.BRStatefield(verbose_name='UF')
     rg = models.CharField(max_length=12, verbose_name='RG')
-    CPF = models.CharField(max_length=12, verbose_name='CPF')
+    cpf = models.CharField(max_length=12, verbose_name='CPF')
     telefone = models.CharField(max_length='10', verbose_name='Telefone')
     celular = models.CharField(max_length='11', verbose_name='Celular')
     SUS_CHOICES = (
-        (u'Sim', u'SIM')
-        (u'Nao', u'NAO')
-
+        (u'S', u'SIM'),
+        (u'N', u'NÃO'),
     )
-convenio_sus = models.CharField(max_length='10', choices=SUS_CHOICES, verbose_name='Conveniado SUS?')
-numero_sus = models.CharField(max_length=12, verbose_name='Numero SUS')
-email = models.CharField(max_length=20, verbose_name='E-mail')
-informacao_menor = models.CharField(max_length=200, verbose_name='Informações do Menor')
+    convenio_sus = models.CharField(max_length='10', choices=SUS_CHOICES, verbose_name='Conveniado SUS?', null=True)
+    numero_sus = models.CharField(max_length=12, verbose_name='Numero SUS', null=True)
+    email = models.CharField(max_length=20, verbose_name='E-mail', null=True)
+    informacao_menor = models.CharField(max_length=200, verbose_name='Informações do Menor', null=True)
+
 
 class medico (models.Model):
     crm = models.CharField(max_length=10, verbose_name='CRM')

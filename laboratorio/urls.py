db7917417ad260b200d069b388f7155d4a7bd752
victor <vitimm144@gmail.com>
2014-09-01 21:32:39 -0300

    Inserido template base da página inicial.
    - Adicionado uma view home
    - Adicionado um pattern para home em urls.py

--- laboratorio/urls.py
+++ laboratorio/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from cadastro.views import home
 from django.contrib import admin
 admin.autodiscover()
 
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'laboratorio.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )

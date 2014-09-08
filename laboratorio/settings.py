c35a0d383c12d72f2d948365588c44a0f4d3ba45
alpombo <alex@insidesolucoes.com.br>
2014-09-08 11:26:49 -0300

    Configuração de static files

--- laboratorio/settings.py
+++ laboratorio/settings.py
@@ -88,3 +88,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+# Additional location of static files
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)

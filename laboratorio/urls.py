6a6ad987fe35bac54b068428d6ee85139dbce43f
alpombo <alex@insidesolucoes.com.br>
2014-09-10 16:39:53 -0300

    Inserido a url do cadastro de médico

--- laboratorio/urls.py
+++ laboratorio/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
-from cadastro.views import home, CadastroPacienteView
+from cadastro.views import home, CadastroPacienteView, CadastroMedicoView
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', home, name='home'),
     url(r'^paciente/new/$', CadastroPacienteView.as_view(), name='new_paciente'),
+    url(r'^medico/new/$', CadastroMedicoView.as_view(), name='new_medico'),
     (r'^login/$', 'login.views.login_user'),
     url(r'^admin/', include(admin.site.urls)),
 )

2e6534da984c9812fe3229ed30bda8b6e29c0cf7
alpombo <alex@insidesolucoes.com.br>
2014-09-10 16:37:32 -0300

    Foi inserido o formulario de cadastro de mádico

--- cadastro/forms.py
+++ cadastro/forms.py
@@ -1,7 +1,12 @@
 from django.forms import ModelForm
 from .models import Paciente
+from .models import Medico
 
 
 class PacienteForm(ModelForm):
     class Meta:
         model = Paciente
+
+class MedicoForm(ModelForm):
+    class Meta:
+        model = Medico

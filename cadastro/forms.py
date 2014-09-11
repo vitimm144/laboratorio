8b28ef61486f30ca8f7225a3bda99c6d182727b9
alpombo <alex@insidesolucoes.com.br>
2014-09-11 13:39:34 -0300

    Foi acrescentado o form referente ao cadastro de Exames

--- cadastro/forms.py
+++ cadastro/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
 from .models import Paciente
 from .models import Medico
-
+from .models import Exame
 
 class PacienteForm(ModelForm):
     class Meta:
@@ -10,3 +10,7 @@ class PacienteForm(ModelForm):
 class MedicoForm(ModelForm):
     class Meta:
         model = Medico
+
+class ExameForm(ModelForm):
+    class Meta:
+        model = Exame

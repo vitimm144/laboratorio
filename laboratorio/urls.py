3374305a800430b019419dad712f481d62c37cf6
alpombo <alex@insidesolucoes.com.br>
2014-09-11 13:40:58 -0300

    Acrescentado a url pertinente ao cadastro de Exame.

--- laboratorio/urls.py
+++ laboratorio/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from cadastro.views import home, CadastroPacienteView, CadastroMedicoView
+from cadastro.views import home, CadastroPacienteView, CadastroMedicoView, CadastroExameView
 from django.contrib import admin
 admin.autodiscover()
 
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     url(r'^$', home, name='home'),
     url(r'^paciente/new/$', CadastroPacienteView.as_view(), name='new_paciente'),
     url(r'^medico/new/$', CadastroMedicoView.as_view(), name='new_medico'),
+    url(r'^exame/new/$', CadastroExameView.as_view(), name='new_exame'),
     (r'^login/$', 'login.views.login_user'),
     url(r'^admin/', include(admin.site.urls)),
 )

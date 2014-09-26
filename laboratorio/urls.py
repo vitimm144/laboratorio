2987737ad11a5adebd660b3d21dfce9603b65963
alpombo <alex@insidesolucoes.com.br>
2014-09-26 15:28:52 -0300

    Criado urls para visualização de lista de pacientes e deletar

--- laboratorio/urls.py
+++ laboratorio/urls.py
@@ -2,15 +2,17 @@ from django.conf.urls import patterns, include, url
 from cadastro.views import home, CadastroExameView, CadastroTexameView, CadastroConvenioView, CadastroAtendimentoView
 from django.contrib import admin
 from cadastro.views import MedicoCreate, MedicoDelete, MedicoUpdate, MedicoListView
-from cadastro.views import PacienteListView, PacienteCreate
+from cadastro.views import PacienteListView, PacienteCreate, PacienteDelete, PacienteUpdate
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', home, name='home'),
     url(r'^paciente/new/$', PacienteCreate.as_view(), name='paciente_new'),
+    url(r'^pacientes/$', PacienteListView.as_view(), name='paciente_list'),
+    url(r'^paciente/(?P<pk>[0-9]+)/$', PacienteUpdate.as_view(), name='paciente_edit'),
+    url(r'^paciente/(?P<pk>[0-9]+)/delete/$', PacienteDelete.as_view(), name='paciente_delete'),
     url(r'^medico/new/$', MedicoCreate.as_view(), name='medico_new'),
     url(r'^medicos/$', MedicoListView.as_view(), name='medico_list'),
-    url(r'^pacientes/$', PacienteListView.as_view(), name='paciente_list'),
     url(r'^medico/(?P<pk>[0-9]+)/$', MedicoUpdate.as_view(), name='medico_edit'),
     url(r'^medico/(?P<pk>[0-9]+)/delete/$', MedicoDelete.as_view(), name='medico_delete'),
     url(r'^exame/new/$', CadastroExameView.as_view(), name='new_exame'),

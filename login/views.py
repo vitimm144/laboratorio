94e6a1e1f0606527256afe7dda10d913378ee1d5
alpombo <alex@insidesolucoes.com.br>
2015-01-09 14:28:58 -0200

    Criado views para cadastro de usuário

--- login/views.py
+++ login/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
 from django.http import *
-from django.shortcuts import render_to_response, redirect
+from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView
+from login.form import UserForm
 
 
 def login_user(request):
@@ -20,3 +22,39 @@ def login_user(request):
                 return HttpResponseRedirect('/')
     return render_to_response('login.html', context_instance=RequestContext(request))
 
+
+#############################
+# Views de Cadastro de Usuário
+#############################
+class UserCreate(CreateView):
+    model = User
+    form_class = UserForm
+    template_name = 'user_form.html'
+
+    def get_success_url(self):
+        return reverse('user_list')
+
+
+class UserUpdate(UpdateView):
+    model = User
+    form_class = UserForm
+    template_name = 'user_form.html'
+
+    def get_success_url(self):
+        return reverse('user_list')
+
+
+class UserDelete(DeleteView):
+    model = User
+    # template_name_suffix = 'templates/'
+    template_name = 'user_confirm_delete.html'
+
+    def get_success_url(self):
+        return reverse('user_list')
+
+
+class UserListView(ListView):
+    model = User
+    template_name = 'user_list.html'
+
+###############################################
